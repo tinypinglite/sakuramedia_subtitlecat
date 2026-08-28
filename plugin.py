3ce5ba5c265a29f66c38d55356cea9e63e70c93f
tinypinglite <tinyping@protonmail.com>
2026-08-28 11:33:07 -0400

    fix(plugin): support Host API 4

--- plugin.py
+++ plugin.py
@@ -7,7 +7,7 @@ from src.plugins import HOST_API_VERSION, PluginContext, PluginRegistration
 from .jobs import build_jobs
 from .settings import SubtitleCatSettings
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 PLUGIN_ID = "sakuramedia_subtitlecat"
 DISPLAY_NAME = "SakuraMedia SubtitleCat 中文字幕"
 
